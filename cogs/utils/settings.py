ea42bf16e11fe797db7e39c598f95c98f0001454
Aamir Farooq <aamir@farooq.xyz>
2020-11-14 15:23:58 +0100

    Changes to settings

--- cogs/utils/settings.py
+++ cogs/utils/settings.py
@@ -5,16 +5,17 @@ from discord.ext import commands
 class Settings(commands.Cog):
     def __init__(self, bot):
         self.guild_id = 777155838849843200
+        guild = bot.get_guild(self.guild_id)
         self.roles = {
-            "Muted": bot.get_guild(self.guild_id).get_role(123345),
-            "MemPlus": bot.get_guild(self.guild_id).get_role(123345),
-            "MemPro" : bot.get_guild(self.guild_id).get_role(123345),
-            "MemEd"  : bot.get_guild(self.guild_id).get_role(123345),
-            "Genius" : bot.get_guild(self.guild_id).get_role(123345),
-            "Mod"    : bot.get_guild(self.guild_id).get_role(123345)
+            "Muted"  : guild.get_role(123345),
+            "MemPlus": guild.get_role(123345),
+            "MemPro" : guild.get_role(123345),
+            "MemEd"  : guild.get_role(123345),
+            "Genius" : guild.get_role(123345),
+            "Mod"    : guild.get_role(123345)
         }
-        self.logging_channel = 688126468517396588
-        self.reports_channel = 688125770186489888
+        self.logging_channel = guild.get_channel(688125770186489888)
+        self.reports_channel = guild.get_channel(688125770186489888)
         self.permissions = Permissions(bot, self.guild_id, self.roles)
 
 
