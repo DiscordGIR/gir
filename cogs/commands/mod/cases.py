f1d5a1982c61b46b8a0bcb4a406769f869387ffc
Aamir Farooq <aamir@farooq.xyz>
2020-11-15 15:41:04 +0100

    Warn command

--- cogs/commands/mod/cases.py
+++ cogs/commands/mod/cases.py
@@ -5,6 +5,8 @@
 # !warnpoints
 import discord
 from discord.ext import commands
+from datetime import datetime
+
 class Cases(commands.Cog):
     def __init__(self, bot):
         self.bot = bot
@@ -12,7 +14,51 @@ class Cases(commands.Cog):
     @commands.command(name="cases")
     async def cases(self, ctx):
         if self.bot.settings.permissions.hasAtLeast(ctx.guild, ctx.author, 6):
-            results = await self.bot.settings.db.get_with_key('users', 'cases')
-            print(results)
+            results = await self.bot.settings.db.get_with_key_and_id('users', 'cases', str(ctx.author.id))
+            await ctx.send(results)
+
+    @commands.command(name="warn")
+    async def warn(self, ctx, user: discord.Member, points: int, *, reason: str = None):
+        case = Case(
+            _id = str(ctx.author.id),
+            _type = "WARN",
+            date = datetime.now().strftime("%m/%d/%Y, %H:%M:%S"),
+            until=str(None),
+            modID=str(ctx.author.id),
+            modTag = str(ctx.author),
+            reason=reason if reason else "No reason",
+            punishment=str(points)
+        )
+        await self.bot.settings.db.append_json("users", "cases", user.id, case.__dict__ )
+        results = await self.bot.settings.db.increment_and_get("users", "warnPoints", user.id, case.punishment)
+        print(results)
+        embed=discord.Embed(title="Warned user")
+        embed.set_author(name=user, icon_url=user.avatar_url)
+        embed.add_field(name="Reason", value=reason, inline=False)
+        embed.add_field(name="Points", value=points, inline=True)
+        embed.add_field(name="Current points", value=results[0]['warnPoints'], inline=True)
+        embed.set_footer(text=f"Warned by {ctx.author}")
+        await ctx.send(embed=embed)        
+
+    @warn.error
+    async def info_error(self, ctx, error):
+        if isinstance(error, commands.MissingRequiredArgument):
+            await ctx.send(error)
+        if isinstance(error, commands.BadArgument):
+            await(ctx.send(error))
+        else:
+            print(error)
+class Case:
+    def __init__(self, _id, _type, date, until, modID, modTag, reason, punishment):
+        self.id         = _id
+        self.type       = _type
+        self.date       = date
+        self.until      = until
+        self.modID      = modID
+        self.modTag     = modTag
+        self.reason     = reason
+        self.punishment = punishment
+
 def setup(bot):
     bot.add_cog(Cases(bot))
+
--- cogs/utils/settings.py
+++ cogs/utils/settings.py
@@ -1,6 +1,7 @@
 import discord
 from discord.ext import commands
 import asyncio
+import json
 
 class Settings(commands.Cog):
     def __init__(self, bot):
@@ -149,8 +150,21 @@ class Database:
         return await stmt.fetch()
 
     async def get_with_key(self, table, key):
-        stmt = await self.conn.prepare(f'SELECT {key} FROM {table}')
+        stmt = await self.conn.prepare(f'SELECT "{key}" FROM {table}')
         return await stmt.fetch()
+        
+    async def get_with_key_and_id(self, table, key, _id):
+        stmt = await self.conn.prepare(f'SELECT "{key}" FROM {table} WHERE id = $1')
+        return await stmt.fetch(str(_id))
+
+    async def append_json(self, table, key, _id, _json):
+        _json = str(json.dumps(_json))
+        print(_json)
+        await self.conn.execute(f'INSERT INTO {table} (id, {key}) VALUES($2, array[$1::json]) ON CONFLICT (id) DO UPDATE SET {key} = EXCLUDED.{key} || $1::json WHERE EXCLUDED.id = $2', _json, str(_id))
+
+    async def increment_and_get(self, table, key, _id, val):
+        await self.conn.execute(f'UPDATE {table} SET "{key}" = "{key}" + $1 WHERE id = $2', int(val), str(_id))
+        return await self.get_with_key_and_id(table, key, _id)
 
 def setup(bot):
     bot.add_cog(Settings(bot))
