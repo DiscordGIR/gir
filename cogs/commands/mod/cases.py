18be5918922c48fd0414b3a4130225f6e367177f
Aamir Farooq <aamir@farooq.xyz>
2020-11-15 16:08:17 +0100

    More work on warn

--- cogs/commands/mod/cases.py
+++ cogs/commands/mod/cases.py
@@ -18,7 +18,13 @@ class Cases(commands.Cog):
             await ctx.send(results)
 
     @commands.command(name="warn")
-    async def warn(self, ctx, user: discord.Member, points: int, *, reason: str = None):
+    async def warn(self, ctx, user: discord.Member, points: int, *, reason: str = "No reason."):
+        if not self.bot.settings.permissions.hasAtLeast(ctx.guild, ctx.author, 6):
+            raise commands.BadArgument("You need to be a moderator or higher to use that command.")
+        if points < 1:
+            raise commands.BadArgument(message="Points can't be lower than 1.")
+        if user.top_role >= ctx.author.top_role:
+            raise commands.BadArgument(message=f"{user}'s top role is the same or higher than yours!")
         case = Case(
             _id = str(ctx.author.id),
             _type = "WARN",
@@ -26,7 +32,7 @@ class Cases(commands.Cog):
             until=str(None),
             modID=str(ctx.author.id),
             modTag = str(ctx.author),
-            reason=reason if reason else "No reason",
+            reason=reason,
             punishment=str(points)
         )
         await self.bot.settings.db.append_json("users", "cases", user.id, case.__dict__ )
@@ -38,13 +44,25 @@ class Cases(commands.Cog):
         embed.add_field(name="Points", value=points, inline=True)
         embed.add_field(name="Current points", value=results[0]['warnPoints'], inline=True)
         embed.set_footer(text=f"Warned by {ctx.author}")
-        await ctx.send(embed=embed)        
+        await ctx.send(embed=embed)  
+        
+        if results[0]['warnPoints'] >= 400 and not results[0]["warnKicked"]:
+            await user.send("You were kicked from r/Jailbreak for reaching 400 or more points.", embed=embed)      
+            await user.kick(reason="400 or more points reached")
+            await self.bot.settings.db.set_with_key_and_id("users", "warnKicked", user.id, True)
+        elif results[0]['warnPoints'] >= 600:
+            await user.send("You were banned from r/Jailbreak for reaching 600 or more points.", embed=embed)      
+            await user.ban(reason="600 or more points reached.")
+        else:
+            await user.send("You were warned in r/Jailbreak.", embed=embed)      
 
     @warn.error
     async def info_error(self, ctx, error):
         if isinstance(error, commands.MissingRequiredArgument):
             await ctx.send(error)
         if isinstance(error, commands.BadArgument):
+            await(ctx.send(error))
+        if isinstance(error, commands.MissingPermissions):
             await(ctx.send(error))
         else:
             print(error)
--- cogs/utils/settings.py
+++ cogs/utils/settings.py
@@ -149,6 +149,10 @@ class Database:
         stmt = await self.conn.prepare(f'SELECT * FROM {table}')
         return await stmt.fetch()
 
+    async def get_with_id(self, table, _id):
+        stmt = await self.conn.prepare(f'SELECT * FROM {table} WHERE id = $1')
+        return await stmt.fetch(str(_id))
+
     async def get_with_key(self, table, key):
         stmt = await self.conn.prepare(f'SELECT "{key}" FROM {table}')
         return await stmt.fetch()
@@ -162,9 +166,12 @@ class Database:
         print(_json)
         await self.conn.execute(f'INSERT INTO {table} (id, {key}) VALUES($2, array[$1::json]) ON CONFLICT (id) DO UPDATE SET {key} = EXCLUDED.{key} || $1::json WHERE EXCLUDED.id = $2', _json, str(_id))
 
+    async def set_with_key_and_id(self, table, key, _id, val):
+        await self.conn.execute(f'UPDATE {table} SET "{key}" = $1 WHERE id = $2', val, str(_id))
+
     async def increment_and_get(self, table, key, _id, val):
         await self.conn.execute(f'UPDATE {table} SET "{key}" = "{key}" + $1 WHERE id = $2', int(val), str(_id))
-        return await self.get_with_key_and_id(table, key, _id)
+        return await self.get_with_id(table, _id)
 
 def setup(bot):
     bot.add_cog(Settings(bot))
