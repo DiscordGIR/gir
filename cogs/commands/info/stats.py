2c7f7de5f5997b5fc3457bda3103464cf9fbcd62
Aamir Farooq <aamir@farooq.xyz>
2020-12-05 02:36:48 +0100

    Improve stas

--- cogs/commands/info/stats.py
+++ cogs/commands/info/stats.py
@@ -4,6 +4,8 @@ import traceback
 from math import floor
 
 import discord
+import psutil
+import platform
 import humanize
 import psutil
 from discord.ext import commands
@@ -36,10 +38,14 @@ class Stats(commands.Cog):
         embed = discord.Embed(
             title=f"{self.bot.user.name} Statistics", color=discord.Color.blurple())
         embed.set_thumbnail(url=self.bot.user.avatar_url)
-        embed.add_field(name="Bot Version", value=discord.__version__)
         embed.add_field(name="Uptime", value=diff)
+        embed.add_field(name="CPU Usage", value=f"{psutil.cpu_percent()}%")
         embed.add_field(name="Memory Usage",
                         value=f"{floor(process.memory_info().rss/1000/1000)} MB")
+        embed.add_field(name="Python Version", value=platform.python_version())
+        embed.add_field(name="disord.py Version", value=discord.__version__)
+        embed.add_field(name="Latency",
+                        value=f"{floor(self.bot.latency)} ms")
 
         await ctx.message.reply(embed=embed)
 
