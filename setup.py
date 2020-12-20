5e347697d40a269340ab93f623edfbc59f91906c
Aamir Farooq <aamir@farooq.xyz>
2020-12-20 16:07:40 +0100

    Updated setup

--- setup.py
+++ setup.py
@@ -14,8 +14,7 @@ async def setup():
     # you should have this setup in the .env file beforehand
     guild._id          = int(os.environ.get("BOTTY_MAINGUILD"))
     guild.nsa_guild_id = int(os.environ.get("BOTTY_NSAGUILD"))
-    guild.case_id      = 1
-
+    guild.case_id      = 11154
     guild.role_mute          = 123  # put in the role IDs for your server here
     guild.role_genius        = 123  # put in the role IDs for your server here
     guild.role_moderator     = 123  # put in the role IDs for your server here
@@ -25,11 +24,14 @@ async def setup():
     guild.role_member        = 123  # put in the role IDs for your server here
     guild.role_dev           = 123  # put in the role IDs for your server here
 
-    guild.channel_public    = 123  # put in the channel IDs for your server here
-    guild.channel_private   = 123  # put in the channel IDs for your server here
-    guild.channel_reports   = 123  # put in the channel IDs for your server here
-    guild.channel_botspam   = 123  # put in the channel IDs for your server here
-    guild.channel_emoji_log = 123  # put in the channel IDs for your server here
+    guild.channel_public         = 123  # put in the channel IDs for your server here
+    guild.channel_private        = 123  # put in the channel IDs for your server here
+    guild.channel_reports        = 123  # put in the channel IDs for your server here
+    guild.channel_botspam        = 123  # put in the channel IDs for your server here
+    guild.channel_emoji_log      = 123  # put in the channel IDs for your server here
+    guild.channel_booster_emoji  = 123  # put in the channel IDs for your server here
+    guild.channel_common_issues  = 123  # put in the channel IDs for your server here
+    guild.channel_reaction_roles = 123  # put in the channel IDs for your server here
    
     guild.logging_excluded_channels = []  # put in a channel if you want (ignored in logging)
     guild.filter_excluded_channels  = []  # put in a channel if you want (ignored in filter)
