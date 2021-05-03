d64623330a252e93731e2d1cac43bd52be8a89c8
Aamir <aamir@farooq.xyz>
2021-05-03 22:18:37 +0200

    Fix nickname filter false positive checking

--- cogs/monitors/filter.py
+++ cogs/monitors/filter.py
@@ -22,6 +22,8 @@ class FilterMonitor(commands.Cog):
             return
         if before.display_name == after.display_name:
             return
+        if self.bot.settings.permissions.hasAtLeast(after.guild, after, 6):
+            return
 
         await self.nick_filter(after)
 
@@ -47,12 +49,27 @@ class FilterMonitor(commands.Cog):
         folded_message = fold(nick.translate(tr).lower()).lower()
         folded_without_spaces = "".join(folded_message.split())
         folded_without_spaces_and_punctuation = folded_without_spaces.translate(str.maketrans('', '', string.punctuation))
-
         if folded_message:
             for word in guild.filter_words:
                 if not self.bot.settings.permissions.hasAtLeast(member.guild, member, word.bypass):
-                    if (word.word.lower() in folded_message or word.word.lower() in folded_without_spaces_and_punctuation):
+                    if (word.word.lower() in folded_message) or \
+                        (not word.false_positive and word.word.lower() in folded_without_spaces) or \
+                        (not word.false_positive and word.word.lower() in folded_without_spaces_and_punctuation):
+                        # remove all whitespace, punctuation in message and run filter again
+                        if word.false_positive and word.word.lower() not in folded_message.split():
+                            continue
                         await member.edit(nick="change name pls", reason=f"filter triggered ({nick})")
+                        await self.do_filter_notify(member, word.word)
+                        return
+    
+    async def do_filter_notify(self, member, word):
+        message = f"Your nickname contained a word you aren't allowed to say in {member.guild.name}. This could be either hate speech or the name of a piracy tool/source. We've automatically changed your name."
+        try:
+            embed = discord.Embed(description=f"{message}\n\nFiltered word found: **{word}**", color=discord.Color.orange())
+            await member.send(embed=embed)
+        except Exception:
+            pass
+
 
    
     async def info_error(self, ctx, error):
