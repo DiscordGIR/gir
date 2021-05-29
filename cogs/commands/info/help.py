540d7df20b820a6bdb689435065650e0d00ef020
Aamir <aamir@farooq.xyz>
2021-05-29 17:56:36 +0200

    Rename this :(

--- cogs/commands/info/help.py
+++ cogs/commands/info/help.py
@@ -10,7 +10,7 @@ class Utilities(commands.Cog):
         self.bot = bot
         self.left_col_length = 17
         self.right_col_length = 80
-        self.mod_only = ["ModActions", "ModUtils", "Filters", "BoosterEmojis", "ReactionRoles", "Giveaway", "Admin", "RaidPhrase"]
+        self.mod_only = ["ModActions", "ModUtils", "Filters", "BoosterEmojis", "ReactionRoles", "Giveaway", "Admin", "AntiRaid"]
         self.genius_only = ["Genius"]
 
     @commands.command(name="help", hidden=True)
