e13649945acccd732970c576c51646851608fe4e
Aamir Farooq <aamir@farooq.xyz>
2021-10-26 21:52:13 +0200

    Added setup instructions

--- main.py
+++ main.py
@@ -9,17 +9,29 @@ from model.guild import Guild
 
 initial_extensions = ["cog"]
 
+
 class Bot(commands.Bot):
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
+        # force the config object to be loaded
         self.guild_id = cfg.guild_id
 
 bot = Bot()
 
 @bot.event
 async def on_ready():
+    logger.info("""                   
+                         _      
+                        (_)     
+                    __ _ _ _ __ 
+                   / _` | | '__|
+                  | (_| | | |   
+                   \__, |_|_|   
+                    __/ |       
+                   |___/            
+                """)
     logger.info(f'\n\nLogged in as: {bot.user.name} - {bot.user.id}\nVersion: {discord.__version__}\n')
-    logger.info(f'Successfully logged in and booted...!')
+
 
 
 if __name__ == '__main__':
