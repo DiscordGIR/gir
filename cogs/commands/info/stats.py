11280b6769cc1559bff04ad8c60193f449646e4b
Aamir Farooq <aamir@farooq.xyz>
2021-10-27 01:14:41 +0200

    roleinfo

--- cogs/commands/info/stats.py
+++ cogs/commands/info/stats.py
@@ -2,9 +2,12 @@ from asyncio import sleep
 from datetime import datetime
 from math import floor
 
-import discord
-from discord.commands import context, slash_command
+from discord.colour import Color
+from discord.commands import slash_command
+from discord.commands.commands import Option
+from discord.embeds import Embed
 from discord.ext import commands
+from discord.role import Role
 from utils.checks import whisper
 from utils.config import cfg
 from utils.context import GIRContext
@@ -15,7 +18,7 @@ class Stats(commands.Cog):
         self.bot = bot
 
     @whisper()
-    @slash_command(guild_ids=[cfg.guild_id])
+    @slash_command(guild_ids=[cfg.guild_id], description="Pong!")
     async def ping(self, ctx: GIRContext) -> None:
         """Pong
 
@@ -25,8 +28,8 @@ class Stats(commands.Cog):
 
         """
 
-        embed = discord.Embed(
-            title="Pong!", color=discord.Color.blurple())
+        embed = Embed(
+            title="Pong!", color=Color.blurple())
         embed.set_thumbnail(url=self.bot.user.display_avatar)
         embed.description = "Latency: testing..."
 
@@ -39,6 +42,16 @@ class Stats(commands.Cog):
         embed.add_field(name="Message Latency", value=f"`{ping}ms`")
         embed.add_field(name="API Latency", value=f"`{floor(self.bot.latency*1000)}ms`")
         await ctx.edit(embed=embed)
+
+    @whisper()
+    @slash_command(guild_ids=[cfg.guild_id], description="Get number of users of a role")
+    async def roleinfo(self, ctx: GIRContext, role: Option(Role, description="Role to view info of")) -> None:
+        embed = Embed(title="Role Statistics")
+        embed.description = f"{len(role.members)} members have role {role.mention}"
+        embed.color = role.color
+        embed.set_footer(text=f"Requested by {ctx.author}")
+
+        await ctx.respond(embed=embed, ephemeral=ctx.whisper)
 
 
 def setup(bot):
